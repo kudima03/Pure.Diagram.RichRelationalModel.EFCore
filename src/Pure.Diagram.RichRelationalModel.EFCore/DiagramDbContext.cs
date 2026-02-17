787fd50b463585b3528f826baeba3472452e2d85
Dmitry Kurochkin <kudima03@gmail.com>
2026-02-17 10:39:11 +0300

    DiagramDbContext implemented

---
+++ src/Pure.Diagram.RichRelationalModel.EFCore/DiagramDbContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Pure.Diagram.RichRelationalModel.EFCore.Models;
+using Pure.Diagram.RichRelationalModel.EFCore.Models.Configurations;
+
+namespace Pure.Diagram.RichRelationalModel.EFCore;
+
+internal sealed class DiagramDbContext(DbContextOptions<DiagramDbContext> options)
+    : DbContext(options)
+{
+    public DbSet<DiagramEFCoreModel> Diagrams => Set<DiagramEFCoreModel>();
+
+    public DbSet<DiagramTypeEFCoreModel> Types => Set<DiagramTypeEFCoreModel>();
+
+    public DbSet<SeriesEFCoreModel> Series => Set<SeriesEFCoreModel>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        _ = modelBuilder.ApplyConfiguration(new SeriesConfiguration());
+        _ = modelBuilder.ApplyConfiguration(new DiagramTypeConfiguration());
+        _ = modelBuilder.ApplyConfiguration(new DiagramConfiguration());
+    }
+}
